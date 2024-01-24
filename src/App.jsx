4659f5de997b66b635eb46f2a83de855822120ae
Ghs Julian <98700347+Ghsjulian@users.noreply.github.com>
2024-01-24 08:15:38 +0000

    home route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter as Router , Route,Routes, BrowserRouter } from 'react-router-dom'
+import Home from "./components/Home"
 import './App.css'
 
 function App() {
