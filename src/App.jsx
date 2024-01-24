e54268ab00ed4b7b1266f00f82d2628d4c88ceb3
Ghs Julian <98700347+Ghsjulian@users.noreply.github.com>
2024-01-24 08:26:00 +0000

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter as Router , Route,Routes, BrowserRouter } from 'react-router-dom'
 import Home from "./components/Home"
 import './App.css'
+import Header from './layouts/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" exact element={<Home/>}/>
     </Routes>
