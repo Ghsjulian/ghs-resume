3ace61feb44c9ea1e7deea53f49c1a3da21e3910
Ghs Julian <98700347+Ghsjulian@users.noreply.github.com>
2024-01-24 08:07:37 +0000

    Header Created

---
+++ src/layouts/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import { Link } from "react-router-dom"
+
+const Header = ()=>{
+    return (
+        <>
+        <div className="header">
+            <Link to="/">Home</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Contact</Link>
+            <Link to="/">Projects</Link>
+            <Link to="/">Resume</Link>
+        </div>
+        </>
+    )
+}
