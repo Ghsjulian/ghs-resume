c431221046cfe1571a4c9398b0abbbcb42b17f7f
Ghs Julian <98700347+Ghsjulian@users.noreply.github.com>
2024-01-24 08:33:56 +0000

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router , Route,Routes, BrowserRouter } from 'react-router-dom'
+import "./App.css"
 import Home from "./components/Home"
-import './App.css'
 import Header from './layouts/Header'
 
 function App() {
